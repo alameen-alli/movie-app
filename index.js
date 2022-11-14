b35abff0f7c0026dff07f435e8d78851901ba40b
Alameen8739 <allialameen8739@gmail.com>
2022-11-14 18:06:22 +0100

    front-end

---
+++ index.js
@@ -0,0 +1,3 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0d9541359facdffff62294e57ea0cf26&page=1';
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=0d9541359facdffff62294e57ea0cf26&query=";
